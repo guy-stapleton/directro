e2f0d670e9fb70d6d7c587346477750accfc4c1b
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-06-04 16:28:09 +1200

    Set up a basic App component

---
+++ client/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
+
+class App extends React.Component {
+  constructor(props) {
+  super(props)
+  }
+  render() {
+    return (
+      <div>
+        <h1>Directo</h1>
+        <h2>Buy Tickets for the Latest Movies</h2>
+      </div>
+    )
+  }
+}
+
+export default App
