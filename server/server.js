d99c441410d8dbd6d93413e59ae2307cf1feb2c2
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-06-04 13:54:56 +1200

    Configure root to send back index.html

--- server/server.js
+++ server/server.js
@@ -4,11 +4,11 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static(__dirname + './public'))
+app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded( { extended: false} ))
 app.get('/', (req, res) => {
-  res.send(`<h1>Hi. How are you?</h1>`)
+  res.sendFile('./index.html')
 })
 
 module.exports = app
