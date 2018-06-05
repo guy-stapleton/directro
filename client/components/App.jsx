73cfdde49b74a36534caa73e80f4a1d523ea74d8
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-06-05 20:50:27 +1200

    Set up login route

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import {HashRouter as Router, Route, Link} from 'react-router-dom'
 
+import Login from './Login'
+
 class App extends React.Component {
   constructor(props) {
   super(props)
   }
   render() {
     return (
-      <div>
-        <h1>Directo</h1>
-        <h2>Buy Tickets for the Latest Movies</h2>
-      </div>
+      <Router>
+        <div>
+          <h1>Directo</h1>
+          <h2>Buy Tickets for the Latest Movies</h2>
+          <Link to={'/login'}>Log in</Link>
+          <Route exact path="/login" component={Login}/>
+        </div>
+    </Router>
     )
   }
 }
